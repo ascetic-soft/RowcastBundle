4734b8567272ccab04956dbddb9c12fffccd4ead
Andrey Borodulin <borodulin@gmail.com>
2026-03-07 01:33:32 +0500

    fix Bundle

--- src/RowcastBundle.php
+++ src/RowcastBundle.php
@@ -4,8 +4,15 @@ declare(strict_types=1);
 
 namespace AsceticSoft\RowcastBundle;
 
+use AsceticSoft\RowcastBundle\DependencyInjection\RowcastExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class RowcastBundle extends Bundle
 {
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new RowcastExtension();
+    }
+
 }
